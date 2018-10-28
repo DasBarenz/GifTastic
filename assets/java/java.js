0194998928693e67982dad7831b06742696b7bae
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-10-27 19:34:43 -0600

    funcationally it's pretty good, buggy adding new items though

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,67 +1,55 @@
 var topics = [
-    "patriot",
-    "Team america",
-    "Old Glory",
-    "freedom",
-    "George Washington",
-    "vote"
+  "Mom",
+  "Team America",
+  "Freedom",
+  "Eagle",
+  "George Washington",
+  "Democracy"
 ]
 
 function makinButtons() {
-  for (i = 0; i < topics.length; i++)
-  var btn = $( "<button>" );
-  btn.text(i);
-  btn.attr("data-gifWord", i)
-  $("#buttons").append();
+  for (i = 0; i < topics.length; i++) {
+    var btn = $("<button>");
+    btn.text(topics[i]);
+    // btn.attr("id", gifButtons)
+    btn.attr("data-gifWord", topics[i])
+    $("#buttons").append(btn);
+  };
 };
 makinButtons();
-//create function to populate buttons on the screen from the array
-//take submitted Gif word, push to array AND re-run the button creation function
 
-$("#submitButton").on("click", function() { 
-    event.preventDefault();
-    var gifSubmission = $("#gifSubmission").val().trim();
-    topics.push(gifSubmission);
-    // var btn = $( "<button>" );
-    // btn.text(gifSubmission);
-    // btn.attr("data-gifWord", gifSubmission)
-    makinButtons();
-    $("#submitButton").trigger("reset")
+$("#submitButton").on("click", function () {
+  event.preventDefault();
+  var gifSubmission = $("#gifSubmission").val().trim();
+  topics.push(gifSubmission);
+  $("#buttons").empty()
+  makinButtons();
+  $("#submitButton").trigger("reset")
 });
 
 
-$("button").on("click", function() {
-    event.preventDefault();
-    var gifWord = $(this).attr("data-gifWord");
-    var queryURL = "https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC&limit=5&tag=" + gifWord;
-        //"https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=20";
-        //"https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=raccoon";
-        //http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC;
-    // topics.push(gifWord);
-    console.log(gifWord);
-    console.log(topics);
+$("button").on("click", function () {
+  event.preventDefault();
+  var gifWord = $(this).attr("data-gifWord");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifWord + "&api_key=dc6zaTOxFJmzC&limit=5";
+  console.log(gifWord);
+  console.log(topics);
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var gifImage = $("<img>");
-          gifImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-          gifDiv.prepend(gifImage);
-
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      });
-  });
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height.url);
+        gifDiv.prepend(p);
+        gifDiv.prepend(gifImage);
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
+});
